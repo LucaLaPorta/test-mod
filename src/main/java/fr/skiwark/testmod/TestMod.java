9542a19053426355e46d87c808688134c06e30a5
Luca La Porta <luca.laporta000@gmail.com>
2022-04-07 15:45:22 +0200

    Initial commit

---
+++ src/main/java/fr/skiwark/testmod/TestMod.java
@@ -0,0 +1,26 @@
+package fr.skiwark.testmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = TestMod.MOD_ID)
+public class TestMod {
+
+    public static final String MOD_ID = "testmod";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+}
